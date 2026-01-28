e1d767d32f8b8b3782b5bd44b39db75f282f7782
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-01-28 02:47:04 +0100

    Byteswap anm files that are embedded into the dol

--- PartyStudioCLI/Program.cs
+++ PartyStudioCLI/Program.cs
@@ -8,25 +8,32 @@ namespace PartyStudioCLI
 {
     internal class Program
     {
-        static string input_dir = @"C:\Users\kabiskac\Documents\git_contributions\marioparty4\orig\GMPE01_00\files\data";
+        static string normal_input_dir = @"C:\Users\kabiskac\Documents\git_contributions\marioparty4\orig\GMPE01_00\files\data";
+        static string dol_extracted_dir = @"C:\Users\kabiskac\Documents\git_contributions\marioparty4\build\GMPE01_00\bin";
         static string output_dir = @"C:\Users\kabiskac\Documents\git_contributions\marioparty4\build\releasex86\GMPE01_00\files\data";
 
         static void Main(string[] args)
         {
-            //output_dir = @"C:\Users\Nathan\Desktop\Games\MP4\data";
-
-            foreach (var file in Directory.GetFiles(input_dir))
+            foreach (var file in Directory.GetFiles(normal_input_dir))
             {
                 if (file.EndsWith(".bin"))
                 {
                     Console.WriteLine("Working on " + file);
-                    BatchByteswap(file);
+                    BatchByteswapBin(file);
+                }
+            }
+            foreach (var file in Directory.GetFiles(dol_extracted_dir))
+            {
+                if (file.EndsWith(".anm"))
+                {
+                    Console.WriteLine("Working on " + file);
+                    BatchByteswapAnm(file);
                 }
             }
             Console.WriteLine("Done");
         }
 
-        static void BatchByteswap(string filepath)
+        static void BatchByteswapBin(string filepath)
         {
             var bin = new MPBIN(filepath);
             for (int i = 0; i < bin.files.Count; i++)
@@ -36,16 +43,12 @@ namespace PartyStudioCLI
                 if (file.FileName.EndsWith(".atb"))
                 {
                     AtbFile atb = new AtbFile(file.FileData);
-                    var mem = new MemoryStream();
-                    atb.Save(mem);
-                    file.FileData = new MemoryStream(mem.ToArray());
+                    SaveAtbToArchive(file, atb);
                 }
                 else if (file.FileName.EndsWith(".hsf"))
                 {
                     HsfFile hsf = new HsfFile(file.FileData);
-                    var mem = new MemoryStream();
-                    hsf.Save(mem);
-                    file.FileData = new MemoryStream(mem.ToArray());
+                    SaveHsfToArchive(file, hsf);
 
                 }
             }
@@ -56,208 +59,20 @@ namespace PartyStudioCLI
             }
         }
 
-        static void ProcessExportAny(string filePath)
+        static void BatchByteswapAnm(string filepath)
         {
-            MPBIN bin = new MPBIN(filePath);
-
-            string folder = Path.Combine("ModelDump", Path.GetFileNameWithoutExtension(filePath));
-
-            for (int i = 0; i < bin.files.Count; i++)
+            var atb = new AtbFile(filepath);
+            string out_file_path = Path.Combine(output_dir, Path.GetFileName(filepath));
+            using (var fileStream = new FileStream(out_file_path, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
             {
-                if (bin.files[i].FileName.Contains("MESH"))
-                {
-                    try
-                    {
-                        ExportModelHSF(bin.files[i], Path.Combine(folder, $"File{i}"));
-                    }
-                    catch
-                    {
-
-                    }
-                }
+                atb.Save(fileStream);
             }
         }
 
-        static void ProcessExport(string filePath, int num, int start_idx = 0)
-        {
-            MPBIN bin = new MPBIN(filePath);
-
-            string folder = Path.Combine("ModelDump", Path.GetFileNameWithoutExtension(filePath));
-
-            for (int i = 0; i < num; i++)
-                ExportModelHSF(bin.files[start_idx + i], Path.Combine(folder, $"LOD{i}"));
-        }
-
-        static void ExportModelHSF(MPBIN.FileEntry file, string folder)
-        {
-            if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
-
-            HsfFile hsf = new HsfFile(file.FileData);
-
-            HSFModelImporter.Export(hsf, Path.Combine(folder, "model.dae"));
-        }
-
-        static void ProcessModelImport(string filePath, string import_file)
-        {
-            string name = Path.GetFileName(filePath);
-
-            MPBIN bin = new MPBIN(filePath);
-
-            //LOD 0 (BOARD & MINI GAME SELECTION SCREEN)
-            ImportModelHSF(bin.files[0], import_file, 0);
-            //LOD 1 (MINI GAMES)
-            ImportModelHSF(bin.files[1], import_file, 1);
-            //LOD 2 (MINI GAMES LOW POLY)
-            ImportModelHSF(bin.files[2], import_file, 2);
-
-            Console.WriteLine($"Compressing BIN");
-
-            bin.Save(Path.Combine(output_dir, name));
-
-            Console.WriteLine($"BIN Saved!");
-        }
-
-        static void ImportModelHSF(MPBIN.FileEntry file, string import_file, int file_idx)
-        {
-            HsfFile hsf = new HsfFile(file.FileData);
-
-            var imported = HSFModelImporter.Import(import_file, hsf, new HSFModelImporter.ImportSettings()
-            {
-
-            });
-
-            hsf.ObjectNodes.Clear();
-            hsf.ObjectNodes.AddRange(imported.ObjectNodes);
-
-            hsf.Meshes.Clear();
-            hsf.Meshes.AddRange(imported.Meshes);
-
-            hsf.Materials.Clear();
-            hsf.Materials.AddRange(imported.Materials);
-
-            hsf.Textures.Clear();
-            hsf.Textures.AddRange(imported.Textures);
-
-            hsf.SkeletonData = imported.SkeletonData;
-            hsf.MatrixData = imported.MatrixData;
-
-            SaveHsfToArchive(file, hsf);
-        }
-
-        static void BatchExportATB(string filePath)
-        {
-            List<AtbFile> files = new List<AtbFile>();
-
-            MPBIN bin = new MPBIN(filePath);
-
-            int atb_file = 0;
-            foreach (var file in bin.Files)
-            {
-                if (file.FileName.EndsWith("atb"))
-                    files.Add(new AtbFile(file.FileData) { FileIndex = atb_file });
-
-                atb_file++;
-            }
-
-            if (files.Count > 0)
-            {
-                string folder = Path.Combine("Dump");
-                string name = Path.GetFileNameWithoutExtension(filePath);
-
-                if (!Directory.Exists(folder))
-                    Directory.CreateDirectory(folder);
-
-                foreach (var atb in files)
-                {
-                    int sprite_idx = 0;
-
-                    foreach (var tex in atb.Textures)
-                    {
-                        string path = Path.Combine(folder, $"{name}_file{atb.FileIndex}_sprite{sprite_idx++}.png");
-                        DumpATB(tex, path);
-
-                        try
-                        {
-                        }
-                        catch
-                        {
-
-                        }
-                    }
-                    atb_file++;
-                }
-            }
-        }
-
-        static void DumpATB(AtbTextureInfo tex, string filePath)
-        {
-            var gcFormat = FormatList[tex.Format & 0xF];
-            var gcnPalette = Decode_Gamecube.PaletteFormats.RGB5A3;
-            var rgba = gctex.Decode(tex.ImageData, tex.Width, tex.Height, (uint)gcFormat,
-                tex.PaletteData, (uint)gcnPalette);
-
-            var image = Image.LoadPixelData<Rgba32>(rgba, tex.Width, tex.Height);
-            image.SaveAsPng(filePath);
-        }
-
-        public static Dictionary<int, Decode_Gamecube.TextureFormats> FormatList = new Dictionary<int, Decode_Gamecube.TextureFormats>()
-            {
-            { 0x00, Decode_Gamecube.TextureFormats.RGBA32 },
-            { 0x01, Decode_Gamecube.TextureFormats.RGB5A3 },
-            { 0x02, Decode_Gamecube.TextureFormats.RGB5A3 },
-            { 0x03, Decode_Gamecube.TextureFormats.C8 },
-            { 0x04, Decode_Gamecube.TextureFormats.C4 },
-            { 0x05, Decode_Gamecube.TextureFormats.IA8 },
-            { 0x06, Decode_Gamecube.TextureFormats.IA4 },
-            { 0x07, Decode_Gamecube.TextureFormats.I8 },
-            { 0x08, Decode_Gamecube.TextureFormats.I4 },
-            { 0x09, Decode_Gamecube.TextureFormats.IA8 },
-            { 0x0A, Decode_Gamecube.TextureFormats.CMPR },
-           };
-
-        static void ProcessMotionBatch(string filePath)
-        {
-            string name = Path.GetFileName(filePath);
-
-            MPBIN bin = new MPBIN(filePath);
-            ProcessMotionBatch(bin);
-
-            Console.WriteLine($"Compressing BIN");
-
-            bin.Save(Path.Combine(output_dir, name));
-
-            Console.WriteLine($"BIN Saved!");
-        }
-
-        static void ProcessMotionBatch(MPBIN bin)
-        {
-            int id = 0;
-            foreach (MPBIN.FileEntry file in bin.Files)
-            {
-                Console.WriteLine($"Processing HSF {id++}");
-                HsfFile hsf = new HsfFile(file.FileData);
-                ProcessMotion(hsf);
-                SaveHsfToArchive(file, hsf);
-            }
-        }
-
-        static void ProcessMotion(HsfFile hsf)
-        {
-            foreach (var motion in hsf.MotionData.Animations)
-            {
-                foreach (var track in motion.GetAllTracks())
-                {
-                    //reset index based motion tracks
-                    //These generally will break custom models, so just blank them out
-                    if (track.TrackMode == TrackMode.Attriubute)
-                        track.ValueIdx = 0;
-                    if (track.TrackMode == TrackMode.Material)
-                        track.ValueIdx = 0;
-
-                    if (track.ValueIdx != 0)
-                        Console.WriteLine($"{track.TrackMode} {(AttributeTrackEffect)track.TrackEffect} Idx {track.Constant}");
-                }
-            }
+        static void SaveAtbToArchive(MPBIN.FileEntry file, AtbFile atb) {
+            var mem = new MemoryStream();
+            atb.Save(mem);
+            file.FileData = new MemoryStream(mem.ToArray());
         }
 
         static void SaveHsfToArchive(MPBIN.FileEntry file, HsfFile hsf)
